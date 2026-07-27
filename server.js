58904debb5f90d3d810082151621e5d627b41763
palakdevspirexinfoways <palak.sales.spirexinfoways@gmail.com>
2026-07-27 13:05:31 +0530

    img uplode size fix 10MB

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ if (!fs.existsSync(uploadsDir)) {
 
 // Standard Request Middlewares (3MB Payload Limit for Uploads)
 app.use(cors());
-app.use(express.json({ limit: '3mb' }));
-app.use(express.urlencoded({ limit: '3mb', extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Serve uploads folder statically
 app.use('/uploads', express.static(uploadsDir));
